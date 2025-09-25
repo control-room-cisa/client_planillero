f585bb38507ef5d91858aae26c8a576ee881763f
iksermd@gmail.com <ikser.marquez@flxgrp.com>
2025-09-25 05:21:52 -0600

    vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: "0.0.0.0",
+    port: 3003,
+    allowedHosts: ["planillero.arrayanhn.com"],
+    strictPort: true,
+  },
+  // Solo si TAMBIÉN quieres acceder al dev server por dominio:
   server: {
-    host: '0.0.0.0', // Permite acceso desde cualquier IP
-    port: 5173
-  }
-})
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: ["planillero.arrayanhn.com"],
+    strictPort: true,
+  },
+});
